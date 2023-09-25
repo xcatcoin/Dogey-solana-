876c0620a47457fa6bc315a3298ead17c77c269e
Alexandria <41755812+Alexandria66@users.noreply.github.com>
2023-09-26 00:01:52 +0700

    Update index.tsx

--- src/games/Mines/index.tsx
+++ src/games/Mines/index.tsx
@@ -4,9 +4,7 @@ import image from './logo.png'
 export default {
   name: 'Mines',
   short_name: 'mines',
-  description: `
-    There's money hidden beneath the squares. The reward will increase the more squares you reveal, but watch out for the 5 hidden mines. Touch one and you'll go broke. You can cash out at any time.
-  `,
+  creator: '6Fa47jDybGSpUX9vZ9NNPV7cfjox9iqUFtpifUSGaPpm',
   theme_color: 'rgb(138 102 255)',
   image,
   app: React.lazy(() => import('./Mines')),
